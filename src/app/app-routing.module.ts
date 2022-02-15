5394f53542472a4a5e60b2fcd0ac0c32ebc59bed
ibkhalili <elkhaliliibrahim65@gmail.com>
2022-02-15 20:05:28 +0100

    pm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', component: WelcomeComponent }
 ];
-
+//loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
